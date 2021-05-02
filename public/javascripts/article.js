b58f5a78b678da26ca115107f1f9e34d9a1a2f48
mrRamezanzad <mohammadrezaramezanzad@gmail.com>
2021-05-02 15:29:26 +0430

    fixed reloading and deletion bugs

--- public/javascripts/article.js
+++ public/javascripts/article.js
@@ -51,7 +51,7 @@ function checkArticleContent () {
 }
 
 // Send Delete Requst
-$("#delete-article").on("click", function (e) {
+$(".delete-article").on("click", function (e) {
   $.ajax({
     type: "DELETE",
     url: `/articles/${$(this).attr("data-article-id")}`,
@@ -63,18 +63,12 @@ $("#delete-article").on("click", function (e) {
         </div>
       `)
 
-      setTimeout(() => {
-        // Decide Going Back To Lists Or Previous Page
-        if(location.pathname.match(/^(\/articles)\/(\w|\d)+/))
-          return history.back(1)
-        location.reload()
-        
-      }, 1500)
+      setTimeout(() => {location.replace("/articles/pages/1")}, 1000)
 
     },
     error: function (err) {
       $(".alert-box").append(`
-        <div class="alert alert-danger text-end alert-dismissible fade show" role="alert">
+        <div class="alert alert-danger text-end aler  t-dismissible fade show" role="alert">
           ${err.responseText}
           <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
         </div>
@@ -109,3 +103,5 @@ $(`.delete-comment`).on('click', function (e) {
     }
   });
 })
+
+// FIXME: MAKE BETTER REDIRECTS
