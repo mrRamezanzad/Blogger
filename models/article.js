ab34b3883b0438a0e404718c490d943439551583
mrRamezanzad <mohammadrezaramezanzad@gmail.com>
2021-05-08 11:54:52 +0430

    -added viewers count feature -fixed comments writer article page link bug

--- models/article.js
+++ models/article.js
@@ -34,6 +34,10 @@ const articleSchema = new mongoose.Schema({
     comments: [{
         type: mongoose.Schema.Types.ObjectId,
         ref: 'Comment' 
+    }],
+    viewers: [{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User',
     }]
 })
 
